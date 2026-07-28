7e934217fc872df556b51d22fb80c08de497eb36
kebe9614 <kebe9614@colorado.edu>
2026-07-28 23:21:29 +0000

    Add database table creation route

--- app.py
+++ app.py
@@ -30,3 +30,36 @@ def db_test():
     finally:
         if conn is not None:
             conn.close()
+
+
+ @app.route("/db_create")
+def db_create():
+    conn = None
+    cur = None
+
+    try:
+        conn = psycopg2.connect(DATABASE_URL)
+        cur = conn.cursor()
+
+        cur.execute("""
+            CREATE TABLE IF NOT EXISTS Basketball(
+                First varchar(255),
+                Last varchar(255),
+                City varchar(255),
+                Name varchar(255),
+                Number int
+            );
+        """)
+
+        conn.commit()
+
+        return "Basketball Table Created"
+
+    except Exception as e:
+        return f"Error creating table: {e}"
+
+    finally:
+        if cur is not None:
+            cur.close()
+        if conn is not None:
+            conn.close()           
